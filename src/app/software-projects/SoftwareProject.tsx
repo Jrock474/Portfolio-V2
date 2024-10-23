adf380f19b74fd6ebb291ff6e9c0ffbd807f8d88
Jordan Williams <jordanw0412@gmail.com>
2024-10-23 15:53:28 -0400

    adjusted component

--- src/app/software-projects/SoftwareProject.tsx
+++ src/app/software-projects/SoftwareProject.tsx
@@ -1,15 +1,16 @@
+import Image from "next/image"
 import { SoftwareDevelopmentProjectProps } from "../interfaceProps"
 
 const Software_Development_Project = (props: SoftwareDevelopmentProjectProps) => {
   return (
     <>
     <div className="software-project">
-      <img src ={props.img} alt={props.title}/>
+      <Image src ={props.img} alt={props.title}/>
       <strong className='software-project-name'>{props.title}</strong>
       <p className='software-project-description'>{props.description}</p>
       <div className='software-button-container'>
-        <a target='_blank' href={props.gitHub}>Github</a>
-        <a target='_blank' href={props.website}>Website</a>  
+        <a target='_blank' rel="noopener noreferrer" href={props.gitHub}>Github</a>
+        <a target='_blank' rel="noopener noreferrer" href={props.website}>Website</a>  
       </div>   
     </div>     
     </>
