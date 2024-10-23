517048b24726bfef9e26a98e5fe751ebc290a4c7
Jordan Williams <jordanw0412@gmail.com>
2024-10-23 14:16:08 -0400

    added and adjusted public folders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 export default function Home() {
   return (
-    <video autoPlay muted loop className="home-page-animation">
-    <source src="videos\Timeline_1.mp4" type="video/mp4" />
-    <p>Sorry, your browser does not support videos.</p>
-</video>
+    <div>hi</div>
   );
 }
