40a1813fad6189172d0021245fc878781fbc864b
Jordan Williams <jordanw0412@gmail.com>
2024-10-27 10:53:33 -0400

    prettier

--- src/app/software-projects/SoftwareProject.tsx
+++ src/app/software-projects/SoftwareProject.tsx
@@ -1,20 +1,26 @@
-import Image from "next/image"
-import { SoftwareDevelopmentProjectProps } from "../interfaceProps"
+import Image from "next/image";
+import { SoftwareDevelopmentProjectProps } from "../interfaceProps";
 
-const Software_Development_Project = (props: SoftwareDevelopmentProjectProps) => {
+const Software_Development_Project = (
+  props: SoftwareDevelopmentProjectProps,
+) => {
   return (
     <>
-    <div className="software-project">
-      <Image src ={props.img} alt={props.title}/>
-      <strong className='software-project-name'>{props.title}</strong>
-      <p className='software-project-description'>{props.description}</p>
-      <div className='software-button-container'>
-        <a target='_blank' rel="noopener noreferrer" href={props.gitHub}>Github</a>
-        <a target='_blank' rel="noopener noreferrer" href={props.website}>Website</a>  
-      </div>   
-    </div>     
+      <div className="software-project">
+        <Image src={props.img} alt={props.title} />
+        <strong className="software-project-name">{props.title}</strong>
+        <p className="software-project-description">{props.description}</p>
+        <div className="software-button-container">
+          <a target="_blank" rel="noopener noreferrer" href={props.gitHub}>
+            Github
+          </a>
+          <a target="_blank" rel="noopener noreferrer" href={props.website}>
+            Website
+          </a>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Software_Development_Project
+export default Software_Development_Project;
