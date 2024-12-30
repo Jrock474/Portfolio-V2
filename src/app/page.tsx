693543d9c32dcad801c3b56e0625b002c1dac760
Jordan Williams <jordanw0412@gmail.com>
2024-12-29 22:22:40 -0500

    updated home page and removed filler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <div>hi</div>;
+  return (
+    <div>
+      <video autoPlay muted loop className="home-page-animation">
+        <source src="videos\Timeline_1.mp4" type="video/mp4" />
+      </video>
+    </div>
+  );
 }
