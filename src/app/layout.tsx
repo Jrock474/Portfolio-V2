e28dc920c7eb32912fc4d1bc2d5201f35b0149d3
Jordan Williams <jordanw0412@gmail.com>
2025-02-08 00:31:56 -0500

    converted 'site-wrapper' css class into tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,8 +26,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-900 text-white min-h-screen flex flex-col relative`}
+        >
         {children}
       </body>
     </html>
