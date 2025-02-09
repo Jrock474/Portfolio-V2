30c13e11bdb8088f9e9923acca49d0bf613f6992
Jordan Williams <jordanw0412@gmail.com>
2025-02-09 15:41:35 -0500

    adjusted footer values and imported header and footer into the layout

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
           <a href="mailto:jordanw0412@gmail.com">Contact</a>
       </p>    
       <p>
-          <Image src="./Site_Files/copyright_symbol_3.png" alt={"Copyright symbol"}/>
+          <Image src="/site_images/copyright_symbol_3.png" alt={"Copyright symbol"} width={25} height={25}/>
           All Rights Reserved
       </p>    
     </footer>
