9735d8bfc60f95f824342b6a51a28986a7fb677b
Jordan Williams <jordanw0412@gmail.com>
2025-02-09 15:41:58 -0500

    prettier

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,17 +1,22 @@
 import Image from "next/image";
 
 const Footer = () => {
-    return (
-        <footer>
+  return (
+    <footer>
       <p>
-          <a href="mailto:jordanw0412@gmail.com">Contact</a>
-      </p>    
+        <a href="mailto:jordanw0412@gmail.com">Contact</a>
+      </p>
       <p>
-          <Image src="/site_images/copyright_symbol_3.png" alt={"Copyright symbol"} width={25} height={25}/>
-          All Rights Reserved
-      </p>    
+        <Image
+          src="/site_images/copyright_symbol_3.png"
+          alt={"Copyright symbol"}
+          width={25}
+          height={25}
+        />
+        All Rights Reserved
+      </p>
     </footer>
-    )
-}
+  );
+};
 
 export default Footer;
