cb6c82b141afb7156e87b5d9f4ab938b5bc8fab9
Jordan Williams <jordanw0412@gmail.com>
2025-02-13 00:05:09 -0500

    created and imported applayout

--- src/app/applayout.tsx
+++ src/app/applayout.tsx
@@ -1,15 +1,22 @@
-export default function appLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-      <html lang="en">
-        <body
-          className="antialiased bg-gray-900 text-white min-h-screen flex flex-col relative"
-        >
-          {children}
-        </body>
-      </html>
-    );
-  }
+"use client";
+import Header from "./header";
+import Footer from "./footer";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+export default function AppLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const theme = createTheme();
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Header />
+        {children}
+        <Footer />
+      </ThemeProvider>
+    </>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import AppLayout from "./applayout";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -28,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-900 text-white min-h-screen flex flex-col relative`}
       >
-        {children}
+        <AppLayout>{children}</AppLayout>
       </body>
     </html>
   );
