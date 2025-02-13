8a19a030a8d63134c58c0883d41c58ba5946e8cd
Jordan Williams <jordanw0412@gmail.com>
2025-02-13 00:35:56 -0500

    code refactoring

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import Link from "next/link";
+import { useTheme, useMediaQuery } from "@mui/material";
 
 const Header = () => {
   const [isNavbarSelected, setIsNavbarSelected] = useState(false);
@@ -13,18 +14,24 @@ const Header = () => {
     }
   };
 
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
   return (
     <>
       <header className="bg-black/30 text-[#f1f1f1] w-full h-auto z-[1000]">
-        <section className="px-0 mx-auto max-w-[96.125rem] w-full h-inherit flex justify-evenly content-center">
-          <div
-            onClick={onNavbarClick}
-            className="absolute hidden flex-col justify-between w-[30px] h-[21px] top-[25px] right-[25px]"
-          >
-            <span className="h-[3px] w-full bg-white rounded-[10px]"></span>
-            <span className="h-[3px] w-full bg-white rounded-[10px]"></span>
-            <span className="h-[3px] w-full bg-white rounded-[10px]"></span>
-          </div>
+        <section className="px-0 mx-auto max-w-[96.125rem] w-full h-inherit flex md:flex-col justify-evenly content-center">
+          {isMobile ? (
+            <div
+              onClick={onNavbarClick}
+              className="absolute hidden flex-col justify-between w-[30px] h-[21px] top-[25px] right-[25px]"
+            >
+              <span className="h-[3px] w-full bg-white rounded-[10px]"></span>
+              <span className="h-[3px] w-full bg-white rounded-[10px]"></span>
+              <span className="h-[3px] w-full bg-white rounded-[10px]"></span>
+            </div>
+          ) : null}
+
           <div className="text-center h-full font-juana">
             <Link href="/">
               <div className="text-[2.5rem]">Jordan</div>
@@ -35,18 +42,14 @@ const Header = () => {
           </div>
           <ul
             className={
-              isNavbarSelected && screen.width <= 1000
+              isNavbarSelected && isMobile
                 ? "flex flex-col w-full p-0 text-center"
                 : "flex list-none items-center"
             }
           >
             <li>
               <Link href="/about">About</Link>
-            </li>
-            <li>
               <Link href="/software-development">Software-Development</Link>
-            </li>
-            <li>
               <Link href="/multi-media">Multi-Media</Link>
             </li>
           </ul>
