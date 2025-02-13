7159fac7f73e181b3bc5df856a53e259c086c764
Jordan Williams <jordanw0412@gmail.com>
2025-02-13 06:48:39 -0500

    adjusted css values for footer

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -2,19 +2,22 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="flex justify-evenly bottom-0 right-0 bg-black/30 text-[#f1f1f1] w-full mt-auto z-[1000] no-underline p-5">
       <p>
         <a href="mailto:jordanw0412@gmail.com">Contact</a>
       </p>
-      <p>
+      <div className="flex flex-row items-center">
         <Image
+        className="h-4 w-4 flex"
           src="/site_images/copyright_symbol_3.png"
           alt={"Copyright symbol"}
-          width={25}
-          height={25}
+          width={16}
+          height={16}
         />
+        <p className="p-3">
         All Rights Reserved
-      </p>
+        </p>
+      </div>
     </footer>
   );
 };
