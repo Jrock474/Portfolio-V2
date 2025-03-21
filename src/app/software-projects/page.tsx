419118e16a195595de81c2231aee0c3f9b01aa41
Jordan Williams <jordanw0412@gmail.com>
2025-03-21 01:34:53 -0400

    updated img attribute to pull src from repository

--- src/app/software-projects/page.tsx
+++ src/app/software-projects/page.tsx
@@ -6,28 +6,28 @@ const page = () => {
       <h1>Software Development Projects</h1>
       <div className="software-development-container">
         <SoftwareProject
-          img="Site_Files/Kanri-Mono server.png"
+          img="/pictures/kanri-mono_server.png"
           title="Kanri-Mono Server"
           gitHub="https://github.com/Jrock474/GraphQL-Practice"
           website="https://graph-ql-practice-nine.vercel.app/"
           description="GraphQL server dedicated to querying the database from Kanri-Mono to return user data"
         />
         <SoftwareProject
-          img="https://raw.githubusercontent.com/Jrock474/Movie_Search/main/src/assets/Movie_Search_Preview.png"
+          img="/pictures/movie_search.png"
           title="Movie Search"
           gitHub="https://github.com/Jrock474/Movie_Search"
           website="https://movie-search-jrock474.vercel.app/"
           description="A mobile responsive React app that searches movies from an IMBD API based off of provided query"
         />
         <SoftwareProject
-          img="https://raw.githubusercontent.com/Jrock474/React_Project/main/src/assets/Screen%20Shot%202023-10-24%20at%209.12.05%20AM.png"
+          img="/pictures/country_search.png"
           title="Country Search"
           gitHub="https://github.com/Jrock474/React_Project"
           website="https://react-project-jrock474.vercel.app/"
           description="My first React Project that searches and displays all Countries from an API while also displaying specific information on the selected country such as it's continent, timezone, ect."
         />
         <SoftwareProject
-          img="https://raw.githubusercontent.com/Jrock474/Austral.github.io/main/Site_Files/Screen%20Shot%202023-10-29%20at%207.19.55%20PM.png"
+          img="/pictures/portfoliov1.png"
           title="Portfolio (Version 1)"
           gitHub="https://github.com/Jrock474/Austral.github.io"
           website="https://jrock474.github.io/Austral.github.io/index.html"
@@ -41,7 +41,7 @@ const page = () => {
           description="Collaborative project focused around creating a pet game. I was responsible for the server and backend"
         />
         <SoftwareProject
-          img="Site_Files/Health & Fitness.png"
+          img="/pictures/health_fitness.png"
           title="Health & Fitness"
           gitHub="https://github.com/Jrock474/GraphQL-Practice"
           website="https://front-end-project-plum.vercel.app/"
