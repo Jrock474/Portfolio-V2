3867b20492d03420d431c160bd128e85029bb4c5
Jordan Williams <jordanw0412@gmail.com>
2025-04-08 17:49:27 -0400

    adjusted project cards and initialized hook

--- src/app/software-projects/SoftwareProject.tsx
+++ src/app/software-projects/SoftwareProject.tsx
@@ -1,33 +1,45 @@
 import Image from "next/image";
+import { useState } from "react";
 import { SoftwareDevelopmentProjectProps } from "./interfaceProps";
 
 const SoftwareProject = (softwareProject: SoftwareDevelopmentProjectProps) => {
+  const [isSleected, setIsSelected] = useState(false);
+
   return (
     <>
       <div className="software-project">
-        <Image width={350} height={350} src={softwareProject.img} alt={softwareProject.title} />
+        <Image
+          width={350}
+          height={350}
+          src={softwareProject.img}
+          alt={softwareProject.title}
+        />
         <strong className="software-project-name">
           {softwareProject.title}
         </strong>
-        <p className="software-project-description">
-          {softwareProject.description}
-        </p>
-        <div className="software-button-container">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href={softwareProject.gitHub}
-          >
-            Github
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href={softwareProject.website}
-          >
-            Website
-          </a>
-        </div>
+        {isSleected ? (
+          <div className="software-button-container">
+            <p className="software-project-description">
+              {softwareProject.description}
+            </p>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={softwareProject.gitHub}
+            >
+              Github
+            </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={softwareProject.website}
+            >
+              Website
+            </a>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     </>
   );
