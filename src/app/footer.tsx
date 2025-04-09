d2dd87c37da1d0b5ad7e8fac6d934309c7159972
Jordan Williams <jordanw0412@gmail.com>
2025-04-09 09:56:10 -0400

    adjusted footer flexbox

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -2,11 +2,9 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="flex justify-evenly bottom-0 right-0 bg-black/30 text-[#f1f1f1] w-full mt-auto z-[1000] no-underline p-5">
-      <p>
-        <a href="mailto:jordanw0412@gmail.com">Contact</a>
-      </p>
+    <footer className="flex justify-between bottom-0 right-0 bg-black/30 text-[#f1f1f1] w-full mt-auto z-[1000] no-underline p-5">
       <div className="flex flex-row items-center">
+      <a href="mailto:jordanw0412@gmail.com">Contact</a>
         <img
           className="h-4 w-4 flex"
           src="/site_images/copyright_symbol_3.png"
