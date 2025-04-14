4c954757d0a3ddeaee895cf6cfbb086bdcd39dca
Jordan Williams <jordanw0412@gmail.com>
2025-04-13 20:08:58 -0400

    added dynamic header and footer logic based on current path of the page

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,8 +1,13 @@
-import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 const Footer = () => {
+
+    const pathName = usePathname();
+
   return (
-    <footer className="flex justify-evenly bottom-0 right-0 bg-black/30 text-[#f1f1f1] w-full mt-auto z-[1000] no-underline p-5">
+    <footer className= {`flex justify-evenly bottom-0 right-0 bg-black/30 text-[#f1f1f1] w-full mt-auto z-[1000] no-underline p-5 ${
+            pathName === "/" ? "bg-black/30" : "bg-[#585757]"
+          }`}>
       <p className="flex items-center">
         <a href="mailto:jordanw0412@gmail.com">Contact</a>
       </p>
