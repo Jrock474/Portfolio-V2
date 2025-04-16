28a1c26e218ae1f62cc4bf521fc7bfadd512709d
Jordan Williams <jordanw0412@gmail.com>
2025-04-16 04:06:08 -0400

    added c sharp

--- src/app/about/SoftwareLanguages.tsx
+++ src/app/about/SoftwareLanguages.tsx
@@ -7,6 +7,7 @@ const SoftwareLanguages = () => {
       <Software img="devicons/CSS3.svg" text="CSS" />
       <Software img="devicons/JavaScript.svg" text="JavaScript" />
       <Software img="devicons/TypeScript.svg" text="Typescript.js" />
+      <Software img="devicons/Csharp.svg" text="C#" />
     </>
   );
 };
