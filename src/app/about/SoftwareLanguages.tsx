d7f511ffa00189836c30a2e8b182c193cab5a05c
Jordan Williams <jordanw0412@gmail.com>
2025-04-16 02:34:09 -0400

    initialized software language component

---
+++ src/app/about/SoftwareLanguages.tsx
@@ -0,0 +1,14 @@
+import Software from "./Software";
+
+const SoftwareLanguages = () => {
+  return (
+    <>
+      <Software img="devicons/HTML5.svg" text="HTML" />
+      <Software img="devicons/CSS3.svg" text="CSS" />
+      <Software img="devicons/JavaScript.svg" text="JavaScript" />
+      <Software img="devicons/TypeScript.svg" text="Typescript.js" />
+    </>
+  );
+};
+
+export default SoftwareLanguages;
