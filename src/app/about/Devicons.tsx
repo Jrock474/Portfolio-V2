37cf6e9697040d240717e00de79bed319108e0c7
Jordan Williams <jordanw0412@gmail.com>
2025-04-17 15:10:39 -0400

    removed languages from devicons

--- src/app/about/Devicons.tsx
+++ src/app/about/Devicons.tsx
@@ -14,12 +14,8 @@ const Devicons = () => {
       <Software img="devicons/Git.svg" text="Git" />
       <Software img="devicons/GitHub.svg" text="GitHub" />
       <Software img="devicons/AWS.svg" text="AWS(EC2)" />
-      <Software img="devicons/HTML5.svg" text="HTML" />
-      <Software img="devicons/CSS3.svg" text="CSS" />
-      <Software img="devicons/JavaScript.svg" text="JavaScript" />
       <Software img="devicons/JSON.svg" text="JSON" />
       <Software img="devicons/React.svg" text="React.js" />
-      <Software img="devicons/TypeScript.svg" text="Typescript.js" />
       <Software img="devicons/Node.js.svg" text="Node.js" />
       <Software img="devicons/Express.svg" text="Express.js" />
       <Software img="devicons/Jest.svg" text="Jest.js" />
