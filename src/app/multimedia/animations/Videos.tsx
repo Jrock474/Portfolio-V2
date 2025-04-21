249abe4254dbe2dc5ed5590a70c5df3726fcc061
Jordan Williams <jordanw0412@gmail.com>
2025-04-20 22:21:29 -0400

    added formatting to videos

--- src/app/multimedia/animations/Videos.tsx
+++ src/app/multimedia/animations/Videos.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Videos = ({ video }: { video: string }) => (
-    <video src={video} controls muted />
+    <video className="max-h-[250px] mx-[5px] mb-[10px] max-w-full " src={video} controls muted />
   );
 
 export default Videos;
