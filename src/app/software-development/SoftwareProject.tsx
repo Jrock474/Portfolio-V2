d2c792bcc39a9e132bb68ab50b007aa875950786
Jordan Williams <jordanw0412@gmail.com>
2025-04-22 21:14:04 -0400

    adjusted overlay

--- src/app/software-development/SoftwareProject.tsx
+++ src/app/software-development/SoftwareProject.tsx
@@ -33,26 +33,28 @@ const SoftwareProject = (softwareProject: SoftwareDevelopmentProjectProps) => {
           alt={softwareProject.title}
         />
         {isSleected ? (
-          <div className="flex flex-col justify-center align-middle width-[100%] absolute h-full">
-            <strong className="flex justify-center w-full">
+          <div className="p-6 bg-black/80 flex text-center flex-col justify-evenly align-middle width-[100%] absolute h-full">
+            <strong className="">
               {softwareProject.title}
             </strong>
             <p> {softwareProject.description}</p>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href={softwareProject.gitHub}
-            >
-              Github
-            </a>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href={softwareProject.website}
-            >
-              Website
-            </a>
+            <div className="flex">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={softwareProject.gitHub}
+              >
+                Github
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={softwareProject.website}
+              >
+                Website
+              </a>
             </div>
+          </div>
         ) : null}
       </div>
     </>
