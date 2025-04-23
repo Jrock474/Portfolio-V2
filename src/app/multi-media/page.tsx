907b2f5086bae8b43a753244947262c01379ad09
Jordan Williams <jordanw0412@gmail.com>
2025-04-22 23:02:17 -0400

    deleted redundant divs

--- src/app/multi-media/page.tsx
+++ src/app/multi-media/page.tsx
@@ -7,19 +7,15 @@ const MultiMedia = () => {
       <div className="gallery-section">
         <div className="flex flex-col min-h-inherit items-center justify-center">
           <Link href="/animations">
-            <div className="about-nav">
-              <div className="no-underline flex justify-center items-center text-center rounded-[2px] min-h-[250px] min-w-[250px] w-auto m-[20px] bg-[rgba(0,0,0,0.6)] border-[1px] border-groove">
-                <p>Animations</p>
-              </div>
+            <div className="no-underline flex justify-center items-center text-center rounded-[2px] min-h-[250px] min-w-[250px] w-auto m-[20px] bg-[rgba(0,0,0,0.6)] border-[1px] border-groove">
+              <p>Animations</p>
             </div>
           </Link>
-          <div className="main-nav">
-            <Link href="/designs">
-              <div className="no-underline flex justify-center items-center text-center rounded-[2px] min-h-[250px] min-w-[250px] w-auto m-[20px] bg-[rgba(0,0,0,0.6)] border-[1px] border-groove">
-                <p>Designs</p>
-              </div>
-            </Link>
-          </div>
+          <Link href="/designs">
+            <div className="no-underline flex justify-center items-center text-center rounded-[2px] min-h-[250px] min-w-[250px] w-auto m-[20px] bg-[rgba(0,0,0,0.6)] border-[1px] border-groove">
+              <p>Designs</p>
+            </div>
+          </Link>
         </div>
       </div>
     </>
