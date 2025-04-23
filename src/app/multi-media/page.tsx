cb86d21f7005d13e44589e8866ca1c2b1a1a0bb1
Jordan Williams <jordanw0412@gmail.com>
2025-04-22 23:07:12 -0400

    deleted redundant div and adjusted flex direction

--- src/app/multi-media/page.tsx
+++ src/app/multi-media/page.tsx
@@ -4,20 +4,18 @@ import PageTitle from "../components/PageTitle";
 const MultiMedia = () => {
   return (
     <>
-      <PageTitle pageTitle="Multi-Media"/>
-      <div className="gallery-section">
-        <div className="flex flex-col min-h-inherit items-center justify-center">
-          <Link href="/animations">
-            <div className="no-underline flex justify-center items-center text-center rounded-[2px] min-h-[250px] min-w-[250px] w-auto m-[20px] bg-[rgba(0,0,0,0.6)] border-[1px] border-groove">
-              <p>Animations</p>
-            </div>
-          </Link>
-          <Link href="/designs">
-            <div className="no-underline flex justify-center items-center text-center rounded-[2px] min-h-[250px] min-w-[250px] w-auto m-[20px] bg-[rgba(0,0,0,0.6)] border-[1px] border-groove">
-              <p>Designs</p>
-            </div>
-          </Link>
-        </div>
+      <PageTitle pageTitle="Multi-Media" />
+      <div className="flex flex-row min-h-inherit items-center justify-center">
+        <Link href="/animations">
+          <div className="no-underline flex justify-center items-center text-center rounded-[2px] min-h-[250px] min-w-[250px] w-auto m-[20px] bg-[rgba(0,0,0,0.6)] border-[1px] border-groove">
+            <p>Animations</p>
+          </div>
+        </Link>
+        <Link href="/designs">
+          <div className="no-underline flex justify-center items-center text-center rounded-[2px] min-h-[250px] min-w-[250px] w-auto m-[20px] bg-[rgba(0,0,0,0.6)] border-[1px] border-groove">
+            <p>Designs</p>
+          </div>
+        </Link>
       </div>
     </>
   );
