aa6055849d1f6ecafc6946b2fc2b5cd47fb2c8fc
Jordan Williams <jordanw0412@gmail.com>
2025-04-23 03:04:15 -0400

    adjusted link path

--- src/app/multi-media/page.tsx
+++ src/app/multi-media/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Link from "next/link";
 import PageTitle from "../components/PageTitle";
 
@@ -11,7 +12,7 @@ const MultiMedia = () => {
             <p>Animations</p>
           </div>
         </Link>
-        <Link href="multi-media/designs">
+        <Link href="multi-media/desgins">
           <div className="no-underline flex justify-center items-center text-center rounded-[2px] min-h-[250px] min-w-[250px] w-auto m-[20px] bg-[rgba(0,0,0,0.6)] border-[1px] border-groove">
             <p>Designs</p>
           </div>
