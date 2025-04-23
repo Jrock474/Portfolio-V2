bd7f6d04b97bbf8dfa64909450dadd9e9047ba18
Jordan Williams <jordanw0412@gmail.com>
2025-04-22 23:01:08 -0400

    converted css to tailwind

--- src/app/multi-media/page.tsx
+++ src/app/multi-media/page.tsx
@@ -5,23 +5,18 @@ const MultiMedia = () => {
     <>
       <h1>MultiMedia</h1>
       <div className="gallery-section">
-        <div className="main-nav-container">
+        <div className="flex flex-col min-h-inherit items-center justify-center">
           <Link href="/animations">
             <div className="about-nav">
-              <div className="main-nav-item">
+              <div className="no-underline flex justify-center items-center text-center rounded-[2px] min-h-[250px] min-w-[250px] w-auto m-[20px] bg-[rgba(0,0,0,0.6)] border-[1px] border-groove">
                 <p>Animations</p>
               </div>
             </div>
           </Link>
           <div className="main-nav">
             <Link href="/designs">
-              <div className="main-nav-item">
+              <div className="no-underline flex justify-center items-center text-center rounded-[2px] min-h-[250px] min-w-[250px] w-auto m-[20px] bg-[rgba(0,0,0,0.6)] border-[1px] border-groove">
                 <p>Designs</p>
-              </div>
-            </Link>
-            <Link href="/music">
-              <div className="main-nav-item">
-                <p>Music</p>
               </div>
             </Link>
           </div>
