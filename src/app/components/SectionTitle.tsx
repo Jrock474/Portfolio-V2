7f9e224c50aaf6a21154ee657a0389bc1d08d59d
Jordan Williams <jordan@Jordans-MacBook-Air.local>
2026-05-31 23:22:31 -0400

    renamed function

--- src/app/components/SectionTitle.tsx
+++ src/app/components/SectionTitle.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const PageTitle = ({ pageTitle }: { pageTitle: string }) => {
+const SectionTitle = ({ pageTitle }: { pageTitle: string }) => {
   return (
     <h2 className="text-center bg-slate-800 text-slate-100 rounded-2xl py-4 px-6 m-0 w-full mb-6 text-2xl font-semibold">
       {pageTitle}
@@ -8,4 +8,4 @@ const PageTitle = ({ pageTitle }: { pageTitle: string }) => {
   );
 };
 
-export default PageTitle;
+export default SectionTitle;
