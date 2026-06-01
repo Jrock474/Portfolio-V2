689f26def9f0132a7bd2adbfb1a2c8ea97e6b032
Jordan Williams <jordan@Jordans-MacBook-Air.local>
2026-05-31 22:43:39 -0400

    updated heading tags

--- src/app/components/PageTitle.tsx
+++ src/app/components/PageTitle.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const PageTitle = ({ pageTitle }: { pageTitle: string }) => {
   return (
-    <h1 className="text-center bg-slate-800 text-slate-100 rounded-2xl py-4 px-6 m-0 w-full mb-6 text-2xl font-semibold">
+    <h3 className="text-center bg-slate-800 text-slate-100 rounded-2xl py-4 px-6 m-0 w-full mb-6 text-2xl font-semibold">
       {pageTitle}
-    </h1>
+    </h3>
   );
 };
 
