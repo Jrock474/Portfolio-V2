2697b735bf7829d6c58933c40fd0eaf8dba3c732
Jordan Williams <jordanw0412@gmail.com>
2026-06-26 02:35:01 -0400

    revert 92d8a09

--- src/app/about/components/SoftwareTechnologies.tsx
+++ src/app/about/components/SoftwareTechnologies.tsx
@@ -4,25 +4,25 @@ const SoftwareTechnologies = () => {
   return (
     <>
       <div className="flex justify-center text-center flex-wrap pb-[50px]">
-        <Software img="/devicons/After Effects.svg" text="After Effects" />
-        <Software img="/devicons/Adobe Illustrator.svg" text="Illustrator" />
-        <Software img="/devicons/Adobe Photoshop.svg" text="Photoshop" />
-        <Software img="/devicons/Jira.svg" text="Jira" />
+        <Software img="devicons/After Effects.svg" text="After Effects" />
+        <Software img="devicons/Adobe Illustrator.svg" text="Illustrator" />
+        <Software img="devicons/Adobe Photoshop.svg" text="Photoshop" />
+        <Software img="devicons/Jira.svg" text="Jira" />
         <Software
-          img="/devicons/Visual Studio Code (VS Code).svg"
+          img="devicons/Visual Studio Code (VS Code).svg"
           text="VS Code"
         />
-        <Software img="/devicons/Git.svg" text="Git" />
-        <Software img="/devicons/GitHub.svg" text="GitHub" />
-        <Software img="/devicons/AWS.svg" text="AWS(EC2)" />
-        <Software img="/devicons/JSON.svg" text="JSON" />
-        <Software img="/devicons/React.svg" text="React.js" />
-        <Software img="/devicons/NodeJS.svg" text="Node.js" />
-        <Software img="/devicons/Express.svg" text="Express.js" />
-        <Software img="/devicons/Jest.svg" text="Jest.js" />
-        <Software img="/devicons/Sequelize.svg" text="Sequelize.js" />
-        <Software img="/devicons/NPM.svg" text="NPM" />
-        <Software img="/devicons/GraphQL.svg" text="GraphQL" />
+        <Software img="devicons/Git.svg" text="Git" />
+        <Software img="devicons/GitHub.svg" text="GitHub" />
+        <Software img="devicons/AWS.svg" text="AWS(EC2)" />
+        <Software img="devicons/JSON.svg" text="JSON" />
+        <Software img="devicons/React.svg" text="React.js" />
+        <Software img="devicons/NodeJS.svg" text="Node.js" />
+        <Software img="devicons/Express.svg" text="Express.js" />
+        <Software img="devicons/Jest.svg" text="Jest.js" />
+        <Software img="devicons/Sequelize.svg" text="Sequelize.js" />
+        <Software img="devicons/NPM.svg" text="NPM" />
+        <Software img="devicons/GraphQL.svg" text="GraphQL" />
       </div>
     </>
   );
